f08013af48bc4b5355896b149b9a74949d61c70c
zaki <codezak75@gmail.com>
2023-11-27 21:19:40 +0100

    finishing the about area

--- js/main.js
+++ js/main.js
@@ -25,6 +25,35 @@ $('document').ready(function(){
         }
         
         no.forEach((el)=> el.style.display = 'none');
-        yes.forEach((el)=> el.style.display = 'block');
+        yes.forEach((el)=> el.style.cssText = 'display:block;animation:zoom 1s ease');
     })
+
+    let clients = $('.about-area .client-row');
+    let radios = $('.about-area .radio')
+    
+    function classSlider(){
+        for(let j=0;j<clients.length;j++){
+            if(clients[j].classList.contains('d-none')){
+                clients[j].classList.remove('d-none');
+            }else{
+                clients[j].classList.add('d-none');
+            }
+        }
+    }
+
+    // setInterval(() => {
+    //     classSlider();
+    // }, 5000);
+
+    for(let i=0;i<=radios.length-1;i++){
+        radios[i].onclick = ()=>{
+            if(i === 1){
+                classSlider();
+            }else{
+                classSlider();
+            }
+            console.log(i);
+        }
+    }
+
 });
